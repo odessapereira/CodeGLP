1f3e1a9307ac566099316a0c60881fcb1f9f7322
farizaamzal <amzalfariza44@gmail.com>
2025-02-14 12:26:38 +0100

    Update BotPlayer.java

--- data/players/BotPlayer.java
+++ data/players/BotPlayer.java
@@ -1,4 +1,35 @@
-package data.players;
+ package data.players;
 
-public class BotPlayer extends Player{
+import data.cards.Card;
+import data.game.Hand;
+import java.util.Random;
+
+public class BotPlayer extends Player {
+
+    // Constructor that initializes the bot with a name, hand, and jackpot
+    public BotPlayer(String name, Hand hand, int jackpot) {
+        super(name, hand, jackpot);
+    }
+
+    // Implementing the abstract method 'playTurn' for the bot to play its turn
+    @Override
+    public void playTurn() {
+        // If the bot has no cards, it can't play anything
+        if (getHand().isEmpty()) {
+            System.out.println(getName() + " has no cards to play.");
+            return;
+        }
+
+        // The bot randomly chooses a card from its hand to play
+        Random rand = new Random();
+        Hand hand = getHand();
+        int randomIndex = rand.nextInt(hand.getCardCount());
+        Card cardToPlay = hand.getCards().get(randomIndex);
+
+        // Remove the card from the hand and play it
+        removeCardFromHand(cardToPlay);
+        System.out.println(getName() + " plays: " + cardToPlay);
+    }
+}
+
 }
