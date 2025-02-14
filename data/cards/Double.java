28d6ebbb52108f819c424b05bd90fdb7f12d53b2
Nadjib <128259571+Nadjibmss@users.noreply.github.com>
2025-02-14 09:53:12 +0100

    Update Double.java

--- data/cards/Double.java
+++ data/cards/Double.java
@@ -1,4 +1,22 @@
 package data.cards;
 
+import java.util.List;
+
 public class Double extends Combinaison{
+
+    public Double (List<Card> cards) {
+        super(cards);
+
+    }
+
+
+    @Override
+    public boolean isValid() {
+        return getCards().size() == 2 && getCards().get(0).getNumber() == getCards().get(1).getNumber();
+    }
+
+    @Override
+    public String toString() {
+        return "Double : " + getCards();
+    }
 }
