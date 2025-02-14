9db9c5714ba8a37ba353904c00f829d0c671d336
Nadjib <128259571+Nadjibmss@users.noreply.github.com>
2025-02-14 09:52:41 +0100

    Update Card.java

--- data/cards/Card.java
+++ data/cards/Card.java
@@ -1,4 +1,25 @@
 package data.cards;
 
-public class Card {
+public class Card{
+
+    private final int number;
+    private final String color;
+
+    public Card (int valeur, String couleur) {
+        this.number=valeur;
+        this.color=couleur;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return number+"de"+color ;
+    }
 }
