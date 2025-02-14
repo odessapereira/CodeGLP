c1762df05d79529cc3a987deb7684cb08d1e892c
odessapereira <odessa.tpereira@icloud.com>
2025-02-14 16:55:25 +0100

    Update Card.java pour l'image

--- data/cards/Card.java
+++ data/cards/Card.java
@@ -4,6 +4,14 @@ public class Card{
 
     private final int number;
     private final String color;
+    private final String imagePath; // Chemin de l'image de la carte
+
+    // Constructeur avec l'ajout d'un chemin d'image
+    public Card(int valeur, String couleur, String imagePath) {
+        this.number = valeur;
+        this.color = couleur;
+        this.imagePath = imagePath; // Le chemin de l'image associé à la carte
+    }
 
     public Card (int valeur, String couleur) {
         this.number=valeur;
