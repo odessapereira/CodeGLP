d709004aca041825cfbff79d1bb479a7cae48e9b
Nadjib <128259571+Nadjibmss@users.noreply.github.com>
2025-02-14 10:11:13 +0100

    Update DiscardPile.java

--- data/game/DiscardPile.java
+++ data/game/DiscardPile.java
@@ -1,4 +1,42 @@
 package data.game;
 
+import data.cards.Card;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class DiscardPile {
+
+    private Stack<Card> playedCards;
+
+    public DiscardPile() {
+        this.playedCards = new Stack<>();
+    }
+
+    public void addCard(Card card) {
+        playedCards.push(card);
+    }
+
+    public void addCards(Stack<Card> cards) {
+        while (!cards.isEmpty()) {
+            playedCards.push(cards.pop());
+        }
+    }
+
+    public Card getLastCard() {
+        return playedCards.isEmpty() ? null : playedCards.peek();
+    }
+
+    public Stack<Card> getPlayedCards() {
+        return (Stack<Card>) playedCards.clone(); // Return a copy to avoid modifications
+    }
+
+    public void clear() {
+        playedCards.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "DiscardPile: " + playedCards;
+    }
 }
