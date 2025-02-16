3de1de134ea95c1ee3a07984945830dadf8a2853
odessapereira <odessa.tpereira@icloud.com>
2025-02-16 11:58:22 +0100

    Update Card.java pour les images

--- data/cards/Card.java
+++ data/cards/Card.java
@@ -7,11 +7,16 @@ public class Card{
     private final String imagePath; // Chemin de l'image de la carte
 
     // Constructeur avec l'ajout d'un chemin d'image
-    public Card(int valeur, String couleur, String imagePath) {
-        this.number = valeur;
-        this.color = couleur;
-        this.imagePath = imagePath; // Le chemin de l'image associé à la carte
+     public Card(int number, String color) {
+        this.number = number;
+        this.color = color.toLowerCase(); // Convertir en minuscules pour correspondre à l'image
+        this.imagePath = generateImagePath(); // Générer le chemin de l'image
     }
+
+    // Méthode pour générer le chemin de l'image basé sur la carte
+    private String generateImagePath() {
+        // Le format de l'image est supposé être "5h.gif" (5 de cœur)
+        return this.number + this.color.charAt(0) + ".gif";
 
     public int getNumber() {
         return number;
